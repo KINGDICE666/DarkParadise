08dc859f263234ff3c4949a6fdeaec9867479582
KINGDICE666 <94010205+KINGDICE666@users.noreply.github.com>
2026-09-14 21:33:54 +0300

    Reuse an already installed bun in the TGS precompile

--- tools/tgs_scripts/PreCompile.sh
+++ tools/tgs_scripts/PreCompile.sh
@@ -9,4 +9,8 @@ set -x
 original_dir=$PWD
 cd "$1"
 
+if [ -x "$HOME/.bun/bin/bun" ]; then
+	export PATH="$HOME/.bun/bin:$PATH"
+fi
+
 env TG_BOOTSTRAP_CACHE="$original_dir" CBT_BUILD_MODE="TGS" tools/bootstrap/javascript.sh tools/build/build.ts
